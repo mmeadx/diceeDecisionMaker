b0763a28062089bb392ac879d66234f2e0b17035
Matt Mead <mattmead@matts-mbp.lan>
2021-01-02 13:20:11 -0600

    added two more dice

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ _A simple dice game using DOM Manipulation - modified code from a lesson in [Ude
 **Click on the 'Roll' button to roll the dice**
 
 
-Using JavaScript and DOM Manipulation the program creates two random numbers and associates them with a corresponding dice image to display on screen. The JavaScript file also uses Else If statements to display the winner (or draw) at the top of the page.
+Using JavaScript and DOM Manipulation the program creates four random numbers and associates them with a corresponding dice image to display on screen. Adding the dice to determine each user's total, the app uses Else If statements to display the winner (or draw) at the top of the page. 
 
 
 Click [HERE](https://mmeadx.github.io/diceeDecisionMaker/) to play
--- index.js
+++ index.js
@@ -3,33 +3,51 @@ console.log("index.js loaded");
 // Choose random numbers 1-6
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+var randomNumber3 = Math.floor((Math.random() * 6) + 1);
+var randomNumber4 = Math.floor((Math.random() * 6) + 1);
 
 // Print in console to check
 console.log(randomNumber1);
 console.log(randomNumber2);
+console.log(randomNumber3);
+console.log(randomNumber4);
 
 // Assign dice names with random number
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-
+var randomDiceImage3 = "dice" + randomNumber3 + ".png";
+var randomDiceImage4 = "dice" + randomNumber4 + ".png";
 
 var randomImageSource1 = "images/" + randomDiceImage1; // images/dice1.png - images/dice6.png
 var randomImageSource2 = "images/" + randomDiceImage2; // images/dice1.png - images/dice6.png
+var randomImageSource3 = "images/" + randomDiceImage3; // images/dice1.png - images/dice6.png
+var randomImageSource4 = "images/" + randomDiceImage4; // images/dice1.png - images/dice6.png
 
 // Select image from html
 var image1 = document.querySelectorAll("img")[0];
 var image2 = document.querySelectorAll("img")[1];
+var image3 = document.querySelectorAll("img")[2];
+var image4 = document.querySelectorAll("img")[3];
 
 // Set Source 
 image1.setAttribute("src", randomImageSource1);
 image2.setAttribute("src", randomImageSource2);
+image3.setAttribute("src", randomImageSource3);
+image4.setAttribute("src", randomImageSource4);
+
+// Add Each User's Dice and print in Console
+var userOneTotal = randomNumber1 + randomNumber3;
+console.log("User One Total: " + userOneTotal);
+var userTwoTotal = randomNumber2 + randomNumber4;
+console.log("User Two Total: " + userTwoTotal);
+
 
 // Else/If to display winner
-if (randomNumber1 > randomNumber2) {
+if (userOneTotal > userTwoTotal) {
     document.querySelector("h1").innerHTML = "User 1 Wins!";
 }
 
-else if (randomNumber2 > randomNumber1) {
+else if (userTwoTotal > userOneTotal) {
     document.querySelector("h1").innerHTML = "User 2 Wins!";
 }
 
